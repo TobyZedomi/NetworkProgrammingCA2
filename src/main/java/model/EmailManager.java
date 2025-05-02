17279c0e61538b97388631e566703016c11b3420
TobyZedomi <tobyzedo4@gmail.com>
2025-05-02 12:50:39 +0100

    Email amanager class methods to add an email, check if email is regex, check if receiver email exist and some bootstrap

--- src/main/java/model/EmailManager.java
+++ src/main/java/model/EmailManager.java
@@ -14,13 +14,36 @@ public class EmailManager {
     public EmailManager() {
 
         bootstrapEmailList();
+
+        bootstrapReceiverEmailList();
     }
 
+    // Add
+
+    public boolean addEmail(String email){
+
+        ArrayList<Email> emailList = new ArrayList<>();
+
+        return add(email, emailList);
+    }
+
+    private boolean add(String email, ArrayList<Email> emailList){
+        boolean added = false;
+        if(!senderEmails.containsKey(email)) {
+            added = true;
+            senderEmails.put(email, emailList);
+            receiverEmails.put(email, emailList);
+        }
+        return added;
+    }
+
+
+
     /// senderEmail an email
-    public boolean sendAnEmailToUser(String sender, String receiver, String subject, String message) {
+    public boolean sendAnEmailToUser(String sender, String receiver, String subject, String message, LocalDateTime dateTime) {
         Email emailToBeSent;
         synchronized (emailCountLock) {
-            emailToBeSent =  new Email(emailIdCount, sender, receiver, subject, message, LocalDateTime.now());
+            emailToBeSent =  new Email(emailIdCount, sender, receiver, subject, message, dateTime);
             emailIdCount++;
         }
         return sendEmail(sender, receiver, emailToBeSent);
@@ -41,27 +64,71 @@ public class EmailManager {
         return false;
     }
 
+
+    // check if receiver exist
+
+    public boolean checkIfReceiverExist(String receiver){
+        boolean match = false;
+        if(receiverEmails.containsKey(receiver)) {
+            match = true;
+        }
+        return match;
+    }
+
+
+    // check if email matches regex
+
+    public boolean checkIfEmailMatchRegex(String email){
+
+        boolean match = false;
+        String pattern = "^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\\.[a-zA-Z]{2,}$";
+        if (email.matches(pattern)){
+
+            match = true;
+        }
+        return match;
+    }
+
     private void bootstrapEmailList() {
         ArrayList<Email> email = new ArrayList();
 
         email.add(new Email(1, "user@gmail.com", "user1@gmail.com", "Hip Hop", "I Love Hip Hop", LocalDateTime.of(2025, 03, 5, 5, 34)));
-        email.add(new Email(2, "user1@gmail.com", "user@gmail.com", "Rock", "I Love Rock", LocalDateTime.of(2025, 02, 9, 5, 34)));
 
         ArrayList<Email> email2 = new ArrayList();
 
         email2.add(new Email(1, "user1@gmail.com", "user2@gmail.com", "Hip Hop", "I Love Hip Hop", LocalDateTime.of(2025, 03, 5, 5, 34)));
-        email2.add(new Email(2, "user2@gmail.com", "user1@gmail.com", "Pop", "I Love Pop", LocalDateTime.of(2025, 02, 9, 5, 34)));
 
 
         ArrayList<Email> email3 = new ArrayList();
 
         email3.add(new Email(1, "user2@gmail.com", "user@gmail.com", "Hip Hop", "I Love Hip Hop", LocalDateTime.of(2025, 03, 5, 5, 34)));
-        email3.add(new Email(2, "user2@gmail.com", "user1@gmail.com", "Pop", "I Love Pop", LocalDateTime.of(2025, 02, 9, 5, 34)));
 
         senderEmails.put("user@gmail.com", email);
         senderEmails.put("user1@gmail.com", email2);
         senderEmails.put("user2@gmail.com", email3);
 
+
+
+    }
+
+    private void bootstrapReceiverEmailList(){
+
+        ArrayList<Email> email = new ArrayList();
+
+        email.add(new Email(1, "user1@gmail.com", "user@gmail.com", "Rock", "I Love Rock", LocalDateTime.of(2025, 02, 9, 5, 34)));
+
+        ArrayList<Email> email2 = new ArrayList();
+
+        email2.add(new Email(1, "user2@gmail.com", "user1@gmail.com", "Pop", "I Love Pop", LocalDateTime.of(2025, 02, 9, 5, 34)));
+
+
+        ArrayList<Email> email3 = new ArrayList();
+
+        email3.add(new Email(1, "user2@gmail.com", "user1@gmail.com", "Pop", "I Love Pop", LocalDateTime.of(2025, 02, 9, 5, 34)));
+
+        receiverEmails.put("user@gmail.com", email);
+        receiverEmails.put("user1@gmail.com", email2);
+        receiverEmails.put("user2@gmail.com", email3);
     }
 
 }
