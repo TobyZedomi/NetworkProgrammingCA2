afcf621ec49e80859e865ba46f85f61362d7c64f
TobyZedomi <tobyzedo4@gmail.com>
2025-05-03 08:59:43 +0100

    AuthUtils for email id doesnt exist and non numeric data

--- src/main/java/client/AuthUtils.java
+++ src/main/java/client/AuthUtils.java
@@ -17,6 +17,9 @@ public class AuthUtils {
 
     public static final String SEARCH_RETRIEVED_EMAILS = "SEARCH_RETRIEVED_EMAILS";
 
+    public static final String GET_CONTENT_RETRIEVED_EMAILS = "GET_CONTENT_RETRIEVED_EMAILS";
+
+
     public static final String ACK = "GOODBYE";
 
     public static final String LOGIN_SUCCESSFUL = "{\"status\":\"LOGIN_SUCCESSFUL\"}";
@@ -24,5 +27,9 @@ public class AuthUtils {
     public static final String NO_EMAILS_WITH_THIS_SUBJECT = "{\"status\":\"NO_EMAILS_WITH_THIS_SUBJECT\"}";
 
     public static final String REGISTER_SUCCESSFUL =   "{\"status\":\"REGISTER_SUCCESSFUL\"}";
+
+    public static final String NON_NUMERIC_ID = "{\"status\":\"NON_NUMERIC_ID\"}";
+
+    public static final String EMAIL_ID_DOESNT_EXIST =   "{\"status\":\"EMAIL_ID_DOESNT_EXIST\"}";
     public static final String DELIMITER = "%%";
 }
